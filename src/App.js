f4be70505ce39f9b590eeab5ce358296c296bf26
Omar <omarnavaalvarez@gmail.com>
2019-03-03 11:48:00 -0500

    final code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Switch, BrowserRouter  } from 'react-router-dom';
+import Home from './Home';
+import Secret from './Secret';
+import Login from './Login';
+import withAuth from './withAuth';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <BrowserRouter >
+                <div>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/secret">Secret</Link></li>
+                    </ul>
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/secret" component={withAuth(Secret)} />
+                        <Route path="/login" component={Login} />
+                    </Switch>
+                </div>
+            </BrowserRouter >
+        );
+    }
 }
-
-export default App;
